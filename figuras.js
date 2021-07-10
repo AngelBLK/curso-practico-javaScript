9089cb652fb1b7489e50b89ab5e5e4928be60939
AngelBLK <jaidenze@gmail.com>
2021-07-10 14:17:19 -0500

    Primer taller del curso, figuras geometricas

---
+++ figuras.js
@@ -0,0 +1,93 @@
+//codifo del cuadrado
+
+const perimetroCuadrado = (lado) => lado * 4;
+const areaCuadrado = (lado) => Math.pow(lado, 2);
+
+//codifo del Triangulo
+
+const perimetroTrinagulo = (lados, base) => lados + lados + base;
+const alturaTriangulo = (lados, base)  => Math.sqrt(((Math.pow(lados, 2))-(Math.pow((base / 2),2))));
+const areaTriangulo = (lados, base) =>  (base * alturaTriangulo(lados, base)) / 2;
+
+//codifo del Circulo
+
+const diametro = (radio) => radio * 2; 
+const perimetroCirculo = (radio) => diametro(radio) * Math.PI;
+const areaCirculo = (radio) => Math.PI * Math.pow(radio, 2);
+
+// Aquí interactuamos con el html.
+
+// Parte del cuadrado.
+const calcularPerimetroCuadrado = () => {
+    const $input = document.getElementById("inputCuadrado"),
+    value = $input.value,
+    $resultado = document.querySelector(".container__form-cuadrado");
+
+    $resultado.innerHTML = `<p> El perimetro del cuadrado es: ${perimetroCuadrado(value)} </p>`;
+}
+
+const calcularAreaCuadrado = () => {
+    const $input = document.getElementById("inputCuadrado"),
+    value = $input.value,
+    $resultado = document.querySelector(".container__form-cuadrado");
+
+    $resultado.innerHTML = `<p> El área del cuadrado es: ${areaCuadrado(value)} </p>`
+}
+
+// Parte del circulo.
+
+const calcularPerimetroCirculo = () => {
+    const $input = document.getElementById("inputCirculo"),
+    value = $input.value,
+    $resultado = document.querySelector(".container__form-circulo");
+
+    $resultado.innerHTML = `<p> El perimetro del cuadrado es: ${perimetroCirculo(value).toFixed(2)} </p>`;
+}
+
+const calcularAreaCirculo = () => {
+    const $input = document.getElementById("inputCirculo"),
+    value = $input.value,
+    $resultado = document.querySelector(".container__form-circulo");
+
+    $resultado.innerHTML = `<p> El área del cuadrado es: ${areaCirculo(value).toFixed(2)} </p>`
+}
+
+const calcularDiametroCirculo = () => {
+    const $input = document.getElementById("inputCirculo"),
+    value = $input.value,
+    $resultado = document.querySelector(".container__form-circulo");
+
+    $resultado.innerHTML = `<p> El área del cuadrado es: ${diametro(value).toFixed(2)} </p>`
+}
+
+// Parte del Triangulo.
+
+const calcularPerimetroTriangulo = () => {
+    const $input = document.getElementById("inputTriangulo"),
+    $base = document.getElementById("inputBase"),
+    value = parseInt($input.value),
+    base = parseInt($base.value),
+    $resultado = document.querySelector(".container__form-triangulo");
+
+    $resultado.innerHTML = `<p> El perimetro del cuadrado es: ${perimetroTrinagulo(value, base)} </p>`;
+}
+
+const calcularAreaTriangulo = () => {
+    const $input = document.getElementById("inputTriangulo"),
+    $base = document.getElementById("inputBase"),
+    value = parseInt($input.value),
+    base = parseInt($base.value),
+    $resultado = document.querySelector(".container__form-triangulo");
+
+    $resultado.innerHTML = `<p> El área del cuadrado es: ${areaTriangulo(value, base).toFixed(2)} </p>`
+}
+
+const calcularAlturaTriangulo = () => {
+    const $input = document.getElementById("inputTriangulo"),
+    $base = document.getElementById("inputBase"),
+    value = parseInt($input.value),
+    base = parseInt($base.value),
+    $resultado = document.querySelector(".container__form-triangulo");
+
+    $resultado.innerHTML = `<p> El área del cuadrado es: ${alturaTriangulo(value, base).toFixed(2)} </p>`
+}
